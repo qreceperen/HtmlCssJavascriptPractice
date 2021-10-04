2dc6582b59cf2f59eef5fef6e4138707bb85b983
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-10-04 11:36:36 -0400

    new files

---
+++ home.js
@@ -0,0 +1,12 @@
+console.log('Hello');
+// alert('Recooooooo')
+
+var firsVariable = 'Howdy'
+console.log(firsVariable);
+
+var someNumber = 45;
+console.log(someNumber)
+
+var num1 = 10;
+
+console.log(250/num1);
